470dc7fa4a04c1bc5c768a99f1ba4fee03124b55
Sumanta Malakar <mlkrsumanta@gmail.com>
2017-06-23 23:56:16 +0530

    Add files via upload

---
+++ Assets/MyGame/MyScripts/PlayerManager Scripts/PlayerManager_HurtCanvas.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Chapter3
+{
+	public class PlayerManager_HurtCanvas : MonoBehaviour 
+	{
+        public GameObject hurtCanvas;
+        private PlayerManager_Master playerManagerMasterScript;
+        private float showCanvasFor = 2;
+
+		void OnEnable()
+		{
+            initiate();
+            playerManagerMasterScript.DecreasePlayerHealthEvent += turnOnHurtEffect;
+		}
+
+		void OnDisable()
+		{
+            playerManagerMasterScript.DecreasePlayerHealthEvent -= turnOnHurtEffect;
+		}
+
+		void initiate()
+		{
+            playerManagerMasterScript =  GetComponent<PlayerManager_Master>();
+		}
+
+        void turnOnHurtEffect(int foo)
+        {
+            if (hurtCanvas != null)
+            {
+                StopAllCoroutines();
+                hurtCanvas.SetActive(true);
+                StartCoroutine(hideCanvasAfter());
+            }
+        }
+
+        IEnumerator hideCanvasAfter()
+        {
+            yield return new WaitForSeconds(showCanvasFor);
+            hurtCanvas.SetActive(false);
+        }
+	}
+
+}
